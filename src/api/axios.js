19216dbe0db2a9cfec0b64c7d11c80369f13a8ef
ramon141 <141barbosa@gmail.com>
2024-07-23 09:23:05 -0300

    usando variaveis de ambiente para api!

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,30 +1,32 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3002"
-})
+  baseURL: process.env.REACT_APP_API_URL,
+});
 
+api.interceptors.request.use(async (config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
 
-api.interceptors.request.use(async config => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
-
-    return config;
-})
+  return config;
+});
 
 api.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        if (error?.response?.status === 401) {
-            localStorage.clear();
-            window.location.href = '/';
-        }
-
-        if (error.code === "ERR_NETWORK")
-            error.response = { data: { message: `A API não está respondendo. Certifique-se que ela está aberta no endereço: ${error.config.baseURL}` } };
-
-        return Promise.reject(error);
+  (response) => response,
+  async (error) => {
+    if (error?.response?.status === 401) {
+      localStorage.clear();
+      window.location.href = "/";
     }
-);
 
+    if (error.code === "ERR_NETWORK")
+      error.response = {
+        data: {
+          message: `A API não está respondendo. Certifique-se que ela está aberta no endereço: ${error.config.baseURL}`,
+        },
+      };
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
